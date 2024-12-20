52a9c75d1830bb61b1bc497a79a8774a12145703
Nick Test <grigoriannick@gmail.com>
2024-12-20 03:18:44 -0500

    reverted to a webapp

--- src/main.rs
+++ src/main.rs
@@ -17,36 +17,28 @@ async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
 
-fn main() {
+/*fn main() {
     let mut b1 = Body::new();
     b1.position.x = 100.0;
     b1.mass = 5.0;
     b1.velocity.z = 1.0;
     let mut b2 = Body::new();
     b2.mass = 1.0;
+    let mut b3 = Body::new();
+    b3.mass = 1.0;
+    b3.position.y = 100.0;
     let mut u = Universe::new();
     u.add_body(b1);
     u.add_body(b2);
-    u.tick_for(1000);
+    u.add_body(b3);
+    u.tick_for(1);
     for body in u.bodies {
         println!("{},{},{}", &body.position.x, &body.position.y, &body.position.z);
     }
-}
+}*/
 
-/*#[actix_web::main]
+#[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let mut b1 = Body::new();
-    b1.position.x = 10.0;
-    b1.mass = 1.0;
-    let mut b2 = Body::new();
-    b2.mass = 1.0;
-    let mut u = Universe::new();
-    u.add_body(b1);
-    u.add_body(b2);
-    u.tick();
-    for body in u.bodies {
-        println!("{},{},{}", &body.position.x, &body.position.y, &body.position.z);
-    }
     HttpServer::new(|| {
         App::new()
             .service(hello)
@@ -56,4 +48,4 @@ async fn main() -> std::io::Result<()> {
     .bind(("127.0.0.1", 8080))?
     .run()
     .await
-}*/
+}
